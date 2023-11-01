2c3af331444a48c6c1808b117250e4ddcb0886c8
VLAD <vlad.spiridovich2015@gmail.com>
2023-11-01 23:43:51 +0300

    Pr2

--- app/src/main/java/ru/mirea/elancev/mireaproject/ui/login/LoginActivity.java
+++ app/src/main/java/ru/mirea/elancev/mireaproject/ui/login/LoginActivity.java
@@ -1,13 +1,15 @@
 package ru.mirea.elancev.mireaproject.ui.login;
 
-
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.StringRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import android.util.Log;
 import android.view.View;
@@ -16,6 +18,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import java.util.List;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -31,10 +34,27 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
     private FirebaseAuth mAuth;
 
+    // Пакетное имя для программы удаленного доступа (AnyDesk)
+    private static final String[] TARGET_APP_PACKAGE = {
+            "com.anydesk.anydeskandroid",
+            "com.microsoft.rdc.androidx",
+            "com.teamviewer.quicksupport.market",
+            "com.google.chromeremotedesktop",
+            "com.sand.airdroid",
+            "com.sand.airmirror",
+            "com.realvnc.viewer.android",
+            "com.microsoft.appmanager"
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Проверяем наличие программы удаленного доступа
+        if (isTargetAppInstalled(TARGET_APP_PACKAGE)) {
+            showTargetAppWarning();
+            return;
+        }
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -45,7 +65,6 @@ public class LoginActivity extends AppCompatActivity {
         final EditText passwordEditText = binding.password;
         final Button loginButton = binding.login;
         final ProgressBar loadingProgressBar = binding.loading;
-
     }
 
     private void showLoginFailed(@StringRes Integer errorString) {
@@ -56,9 +75,40 @@ public class LoginActivity extends AppCompatActivity {
         return true;
     }
 
+    private boolean isTargetAppInstalled(String[] targetAppPackages) {
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> installedApps = packageManager.getInstalledApplications(0);
+
+        for (ApplicationInfo appInfo : installedApps) {
+            String packageName = appInfo.packageName;
+            for (String targetAppPackage : targetAppPackages) {
+                if (packageName.equals(targetAppPackage)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private void showTargetAppWarning() {
+        new AlertDialog.Builder(this)
+                .setTitle("Предупреждение")
+                .setMessage("Обнаружена программа удаленного доступа (AnyDesk). Вы должны удалить ее, чтобы продолжить использование этого приложения.")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Закрываем приложение
+                        finish();
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
+
     private boolean createAccount(String email, String password) {
         Log.d(TAG, "createAccount:" + email);
-        final Boolean[] flag = {false};
         if (!validateForm()) {
             return false;
         }
@@ -69,28 +119,25 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            flag[0] = true;
                             System.out.println("SUCCESS");
 
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish();
                         } else {
-
-                            Log.w(TAG, "createUserWithEmail:failure",
-                                    task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
-        System.out.println("register " + flag[0]);
-        return flag[0];
+        return true;
     }
 
     private boolean signIn(String email, String password) {
-        final Boolean[] flag = {false};
         Log.d(TAG, "signIn:" + email);
+        if (!validateForm()) {
+            return false;
+        }
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -98,37 +145,26 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            flag[0] = true;
                             System.out.println("SUCCESS");
 
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish();
                         } else {
-
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                         }
-
-                        if (!task.isSuccessful()) {
-
-                        }
-
-
                     }
                 });
-        System.out.println("sign in " + flag[0]);
-        return flag[0];
+        return true;
     }
 
     public void clickLog(View v) {
-        System.out.println(binding.username.getText().toString() + " " + binding.password.getText().toString());
-        if (signIn(binding.username.getText().toString(),
-                binding.password.getText().toString()) ||
-                createAccount(binding.username.getText().toString(),
-                        binding.password.getText().toString())) {
+        String email = binding.username.getText().toString();
+        String password = binding.password.getText().toString();
+
+        if (signIn(email, password) || createAccount(email, password)) {
+            // Действия после успешной авторизации
         }
     }
 }
