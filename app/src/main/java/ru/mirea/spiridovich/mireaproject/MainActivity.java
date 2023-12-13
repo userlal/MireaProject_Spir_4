6930e1b338629f0957854ad6974295ee638aff17
VLAD <vlad.spiridovich2015@gmail.com>
2023-12-13 17:08:26 +0300

    pr4

--- app/src/main/java/ru/mirea/spiridovich/mireaproject/MainActivity.java
+++ app/src/main/java/ru/mirea/spiridovich/mireaproject/MainActivity.java
@@ -1,17 +1,22 @@
-package ru.mirea.spiridovich.mireaproject;
+package ru.mirea.spiridovich.mireaproject;import android.app.AlertDialog;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.concurrent.TimeUnit;
+
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import ru.mirea.spiridovich.mireaproject.databinding.ActivityMainBinding;
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    private final Handler handler = new Handler(Looper.getMainLooper());
+    private static final long IDLE_TIMEOUT = TimeUnit.SECONDS.toMillis(15); // 15 секунд бездействия
+    private Runnable idleRunnable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,30 +40,45 @@ public class MainActivity extends AppCompatActivity {
         binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Сброс таймера при каждом взаимодействии пользователя
+                resetIdleTimer();
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Отложенное предупреждение и закрытие приложения
+        idleRunnable = new Runnable() {
+            @Override
+            public void run() {
+                showIdleAlertDialog();
+            }
+        };
+
+        binding.drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                resetIdleTimer(); // Сброс таймера при открытии Navigation Drawer
+            }
+        });
 
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_browser, R.id.nav_fragment)
-                .setOpenableLayout(drawer)
+                .setOpenableLayout(binding.drawerLayout)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    protected void onStart() {
+        super.onStart();
+        // Начать таймер бездействия при запуске
+        resetIdleTimer();
     }
+
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -63,4 +86,23 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+
+    private void resetIdleTimer() {
+        handler.removeCallbacks(idleRunnable);
+        handler.postDelayed(idleRunnable, IDLE_TIMEOUT);
+    }
+
+    private void showIdleAlertDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Вы неактивны в течение 15 секунд. Приложение будет закрыто.")
+                .setPositiveButton("ОК", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish(); // Закрыть приложение
+                    }
+                })
+                .setCancelable(false)
+                .show();
+    }
 }
+
